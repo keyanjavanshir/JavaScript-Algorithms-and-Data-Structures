570a65761cee94d66799eef2b702f527a411b4b7
Keyan <keyanjavanshir@live.dk>
2022-02-12 14:50:20 +0100

    ssfa

---
+++ scripts/start.js
@@ -0,0 +1,103 @@
+// Declaring variables
+var myName
+let anotherName
+
+// Storing values
+var number
+number = 7
+
+// Assigning values to of one variable to another
+var myWorld
+myWorld = "World"
+
+var myHello
+myHello = "Hello" + ", " + myWorld + "!"
+
+
+// Initializing variables with the Assignment Operator
+var a = 9
+
+// Declare String Variables
+var myFirstName = "Keyan"
+var myLastName = "Javanshir"
+
+// Understanding Uninitialized Variables
+var a = 5
+var b = 10
+var c = "I am a"
+
+// Explore Differences Between the var and let Keywords
+let catName = "Oliver";
+let catSound = "Meow!";
+
+// Unlike var, when you use let, a variable with the same name can only be declared once.
+
+// Declare a Read-Only Variable with the const Keyword
+const FCC = "freeCodeCamp" // Common to use UPPERCASE for const variables
+let fact = "is cool!"
+fact = "is awesome!" 
+
+/* Const values are as the name implies.. constant. That means they remain unchangable after assigning a value.
+
+Another note is that to use uppercase notation for immutable values, and lowercase/camelcase for mutable values.*/
+
+// Add Two Numbers with JavaScript
+const sum = 10 + 10
+
+// Subtract One Number from Another with JavaScript
+const difference = 45-33
+
+// Multiply Two Numbers with JavaScript
+const product = 8*10
+
+// Divide One Number by Another with JavaScript
+const quotient = 66/33
+
+// Increment a Number with JavaScript
+let myInc = 87
+myInc++
+
+// Decrement a Number with JavaScript
+let myDec = 11
+myDec--
+
+// Create Decimal Numbers with JavaScript
+let myDecimal = 5.7
+
+// Multiply Two Decimals with JavaScript
+const doubleDec = 10.5 * 5.2
+
+// Divide One Decimal by Another with JavaScript
+const doubleQuot = 4.4 / 2.0
+
+// Finding a Remainder in JavaScript
+const remainder = 11%3
+
+// Compound Assignment With Augmented Addition
+let d = 8
+d += 9
+
+// Compound Assignment With Augmented Subtraction
+d -= 9
+
+// Compound Assignment With Augmented Multiplication
+d *= 5
+
+// Compound Assignment With Augmented Division
+d /= 3
+
+// Escaping Literal Quotes in Strings
+let myStr = "I am a \"double quoted\" string inside \"double quotes\"."
+
+// Quoting Strings with Single Quotes
+// const myStr = "<a href=\"http://www.example.com\" target=\"_blank\">Link</a>";
+
+let myLink = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+// Escape Sequences in Strings
+let myEscape = 
+
+
+
+
+// Run using node scripts/start.js
